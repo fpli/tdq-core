cd725367abe0c833fa770aa93da02ada5220fb22
Jason Xiao <yxiao6@ebay.com>
2020-01-08 11:24:11 +0800

    fix test compile issue

--- common/src/test/java/com/ebay/sojourner/ubd/common/metrics/EventCntMetricsTest.java
+++ common/src/test/java/com/ebay/sojourner/ubd/common/metrics/EventCntMetricsTest.java
@@ -2,26 +2,24 @@ package com.ebay.sojourner.ubd.common.metrics;
 
 import com.ebay.sojourner.ubd.common.sharedlib.metrics.EventCntMetrics;
 import com.fasterxml.jackson.databind.JsonNode;
-import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 import java.util.Collection;
-import java.util.List;
 
-public class EventCntMetricsTest extends BaseMetricsTest{
+public class EventCntMetricsTest extends BaseMetricsTest {
     private EventCntMetrics eventCntMetrics;
-    private Pair<JsonNode, List<MetricsTestCase>> pair;
+    private JsonNode yaml;
 
     @BeforeEach
     public void setup() throws Exception {
         eventCntMetrics = new EventCntMetrics();
-        pair = loadTestCases("EventCntMetricTest.yaml");
+        yaml = loadTestCasesYaml("EventCntMetricTest.yaml");
     }
 
     @TestFactory
     public Collection<DynamicTest> dynamicTests() throws Exception {
-        return generateDynamicTests(pair.getRight(), pair.getLeft(), eventCntMetrics);
+        return generateDynamicTests(yaml, eventCntMetrics);
     }
 }
