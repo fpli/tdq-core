bc0cc88878211649983afc1d17f72d9cbf04f090
Jason Xiao <yxiao6@ebay.com>
2020-01-14 17:48:16 +0800

    add support to pass args via command line

--- rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/pipeline/SojournerUBDRTJobUntilSession.java
+++ rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/pipeline/SojournerUBDRTJobUntilSession.java
@@ -11,9 +11,8 @@ import com.ebay.sojourner.ubd.rt.operators.event.UbiEventMapWithStateFunction;
 import com.ebay.sojourner.ubd.rt.operators.session.UbiSessionAgg;
 import com.ebay.sojourner.ubd.rt.operators.session.UbiSessionWindowProcessFunction;
 import com.ebay.sojourner.ubd.rt.util.AppEnv;
-import org.apache.flink.api.common.typeinfo.SOjStringFactory;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.java.typeutils.TypeExtractor;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -26,12 +25,12 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.runtime.operators.windowing.WindowOperatorHelper;
 import org.apache.flink.util.OutputTag;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
-
 public class SojournerUBDRTJobUntilSession {
 
     public static void main(String[] args) throws Exception {
+        // Make sure this is being executed at start up.
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        AppEnv.config(parameterTool);
 
         // hack StringValue to use the version 1.10
 //        Method m = TypeExtractor.class.getDeclaredMethod("registerFactory", Type.class, Class.class);
