97485d83caf995e9c65123c833ff38b5115dcdad
haxi <James_boge@163.com>
2020-01-14 12:24:51 +0800

    add some unit test

---
+++ common/src/test/java/com/ebay/sojourner/ubd/common/attributes/AgentIpAttributeTest.java
@@ -0,0 +1,6 @@
+package com.ebay.sojourner.ubd.common.attributes;
+
+import com.ebay.sojourner.ubd.common.model.UbiSession;
+
+public class AgentIpAttributeTest extends BaseAttributesTest<UbiSession> {
+}
