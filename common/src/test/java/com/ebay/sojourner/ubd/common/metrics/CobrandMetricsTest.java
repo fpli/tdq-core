e11beea75be8c1304c9d471472cff84426965b6c
haxi <James_boge@163.com>
2020-01-15 14:13:54 +0800

    add some metrics unit test

--- common/src/test/java/com/ebay/sojourner/ubd/common/metrics/CobrandMetricsTest.java
+++ common/src/test/java/com/ebay/sojourner/ubd/common/metrics/CobrandMetricsTest.java
@@ -1,6 +1,6 @@
 package com.ebay.sojourner.ubd.common.metrics;
 
-import com.ebay.sojourner.ubd.common.sharedlib.metrics.AgentStringMetrics;
+import com.ebay.sojourner.ubd.common.sharedlib.metrics.CobrandMetrics;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DynamicTest;
@@ -8,18 +8,18 @@ import org.junit.jupiter.api.TestFactory;
 
 import java.util.Collection;
 
-public class AgentStringMetricsTest extends BaseMetricsTest {
-    private AgentStringMetrics agentStringMetrics;
+public class CobrandMetricsTest extends BaseMetricsTest {
+    private CobrandMetrics cobrandMetrics;
     private JsonNode yaml;
 
     @BeforeEach
     public void setup() throws Exception {
-        agentStringMetrics = new AgentStringMetrics();
-        yaml = loadTestCasesYaml("AgentStringMetricsTest.yaml");
+        cobrandMetrics = new CobrandMetrics();
+        yaml = loadTestCasesYaml("CobrandMetricsTest.yaml");
     }
 
     @TestFactory
     public Collection<DynamicTest> dynamicTests() throws Exception {
-        return generateDynamicTests(yaml, agentStringMetrics);
+        return generateDynamicTests(yaml, cobrandMetrics);
     }
 }
--- common/src/test/java/com/ebay/sojourner/ubd/common/metrics/SiidCntMetricsTest.java
+++ common/src/test/java/com/ebay/sojourner/ubd/common/metrics/SiidCntMetricsTest.java
@@ -1,6 +1,6 @@
 package com.ebay.sojourner.ubd.common.metrics;
 
-import com.ebay.sojourner.ubd.common.sharedlib.metrics.AgentStringMetrics;
+import com.ebay.sojourner.ubd.common.sharedlib.metrics.SiidCntMetrics;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DynamicTest;
@@ -8,18 +8,18 @@ import org.junit.jupiter.api.TestFactory;
 
 import java.util.Collection;
 
-public class AgentStringMetricsTest extends BaseMetricsTest {
-    private AgentStringMetrics agentStringMetrics;
+public class SiidCntMetricsTest extends BaseMetricsTest {
+    private SiidCntMetrics siidCntMetrics;
     private JsonNode yaml;
 
     @BeforeEach
     public void setup() throws Exception {
-        agentStringMetrics = new AgentStringMetrics();
-        yaml = loadTestCasesYaml("AgentStringMetricsTest.yaml");
+        siidCntMetrics = new SiidCntMetrics();
+        yaml = loadTestCasesYaml("SiidCntMetricsTest.yaml");
     }
 
     @TestFactory
     public Collection<DynamicTest> dynamicTests() throws Exception {
-        return generateDynamicTests(yaml, agentStringMetrics);
+        return generateDynamicTests(yaml, siidCntMetrics);
     }
 }
