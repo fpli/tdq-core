c74c447f541f45ea32afa58f7cc4b9277d8cedc3
haxi <James_boge@163.com>
2020-03-10 17:56:56 +0800

    fix histogram metrics

--- rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/operators/event/EventDiscardingSink.java
+++ rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/operators/event/EventDiscardingSink.java
@@ -10,37 +10,38 @@ import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 
 public class EventDiscardingSink extends RichSinkFunction<UbiEvent> {
 
-  private transient DropwizardHistogramWrapper siteToSourceHistogram;
-  private transient DropwizardHistogramWrapper siteToSinkHistogram;
-  private transient DropwizardHistogramWrapper sourceToSinkHistogram;
+  private transient DropwizardHistogramWrapper siteToSourceWrapper;
+  private transient DropwizardHistogramWrapper siteToSinkWrapper;
+  private transient DropwizardHistogramWrapper sourceToSinkWrapper;
 
   @Override
   public void open(Configuration conf) throws Exception {
     super.open(conf);
-    Histogram dropwizardHistogram =
+    Histogram siteToSourceHistogram =
         new Histogram(new SlidingWindowReservoir(500));
-    siteToSourceHistogram = getRuntimeContext().getMetricGroup()
+    Histogram siteToSinkHistogram =
+        new Histogram(new SlidingWindowReservoir(500));
+    Histogram sourceToSinkHistogram =
+        new Histogram(new SlidingWindowReservoir(500));
+    siteToSourceWrapper = getRuntimeContext().getMetricGroup()
         .addGroup("sojourner-ubd")
-        .histogram("site to source", new DropwizardHistogramWrapper(dropwizardHistogram));
-    siteToSinkHistogram = getRuntimeContext().getMetricGroup()
+        .histogram("site to source", new DropwizardHistogramWrapper(siteToSourceHistogram));
+    siteToSinkWrapper = getRuntimeContext().getMetricGroup()
         .addGroup("sojourner-ubd")
-        .histogram("site to sink", new DropwizardHistogramWrapper(dropwizardHistogram));
-    sourceToSinkHistogram = getRuntimeContext().getMetricGroup()
+        .histogram("site to sink", new DropwizardHistogramWrapper(siteToSinkHistogram));
+    sourceToSinkWrapper = getRuntimeContext().getMetricGroup()
         .addGroup("sojourner-ubd")
-        .histogram("source to sink", new DropwizardHistogramWrapper(dropwizardHistogram));
+        .histogram("source to sink", new DropwizardHistogramWrapper(sourceToSinkHistogram));
   }
 
   @Override
   public void invoke(UbiEvent value, Context context) throws Exception {
     long end = new Date().getTime();
-    System.out.println("endTime==============="+end);
-    System.out.println("ingestTime============"+value.getIngestTime());
-    System.out.println("generateTime=========="+value.getGenerateTime());
     long siteToSource = value.getIngestTime() - value.getGenerateTime();
     long siteToSink = end - value.getGenerateTime();
     long sourceToSink = (end - value.getIngestTime());
-    siteToSinkHistogram.update(siteToSink);
-    siteToSourceHistogram.update(siteToSource);
-    sourceToSinkHistogram.update(sourceToSink);
+    siteToSourceWrapper.update(siteToSink);
+    siteToSinkWrapper.update(siteToSource);
+    sourceToSinkWrapper.update(sourceToSink);
   }
 }
