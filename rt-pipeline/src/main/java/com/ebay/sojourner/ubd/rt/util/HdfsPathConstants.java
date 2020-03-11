b278d67bf460853ec29fa647982bd9757c360acd
haxi <James_boge@163.com>
2020-03-11 18:45:27 +0800

    change hdfs sink path to /sys/soj

--- rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/util/HdfsPathConstants.java
+++ rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/util/HdfsPathConstants.java
@@ -2,8 +2,8 @@ package com.ebay.sojourner.ubd.rt.util;
 
 public class HdfsPathConstants {
 
-  public static final String EVENT_PATH = "hdfs://apollo-rno//user/o_ubi/events/";
-  public static final String SESSION_PATH = "hdfs://apollo-rno//user/o_ubi/sessions/";
-  public static final String SIGNATURE_PATH = "hdfs://apollo-rno//user/o_ubi/signatures/";
-  public static final String LATE_EVENT_PATH = "hdfs://apollo-rno//user/o_ubi/late_events/";
+  public static final String EVENT_PATH = "hdfs://apollo-rno//sys/soj/events/";
+  public static final String SESSION_PATH = "hdfs://apollo-rno//sys/soj/sessions/";
+  public static final String SIGNATURE_PATH = "hdfs://apollo-rno//sys/soj/signatures/";
+  public static final String LATE_EVENT_PATH = "hdfs://apollo-rno//sys/soj/late_events/";
 }
