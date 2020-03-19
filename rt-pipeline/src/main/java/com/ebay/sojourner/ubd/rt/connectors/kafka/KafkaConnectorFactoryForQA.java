bc89704285f9c5202549b6e9cc3a878fbf2a1ec7
haxi <James_boge@163.com>
2020-03-19 14:55:08 +0800

    add QA main class for local test

--- rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/connectors/kafka/KafkaConnectorFactoryForQA.java
+++ rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/connectors/kafka/KafkaConnectorFactoryForQA.java
@@ -12,12 +12,12 @@ import org.apache.kafka.clients.consumer.RoundRobinAssignor;
 import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 
-public class KafkaConnectorFactoryForSOJ {
+public class KafkaConnectorFactoryForQA {
 
   public static final String GROUP_ID = AppEnv.config().getKafka().getGroupId();
   public static final String TOPIC_PATHFINDER_EVENTS = AppEnv.config().getKafka().getTopic();
   public static final String BOOTSTRAP_SERVERS =
-      String.join(",", AppEnv.config().getKafka().getBootstrapServersForSOJ());
+      String.join(",", AppEnv.config().getKafka().getBootstrapServersForQA());
 
   public static FlinkKafkaConsumer<RawEvent> createKafkaConsumer() {
     Properties props = new Properties();
@@ -37,23 +37,15 @@ public class KafkaConnectorFactoryForSOJ {
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
     props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
 
-    props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 3000);
-    props.put(ConsumerConfig.RECEIVE_BUFFER_CONFIG, 8 * 1024 * 1024);
-    props.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 250 * 1024 * 1024);
-    props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 1000);
-
-    props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, 8 * 1024 * 1024);
-
     props.put(
         ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, RoundRobinAssignor.class.getName());
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
     props.put(
         ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, RheosEventDeserializer.class.getName());
-    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-    //        props.put(ConsumerConfig.CLIENT_ID_CONFIG, CLIENT_ID);
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
     return new FlinkKafkaConsumer<>(
-        TOPIC_PATHFINDER_EVENTS, new SojEventDeserializationSchema(), props);
+        TOPIC_PATHFINDER_EVENTS, new RawEventDeserializationSchemaForRNO(), props);
   }
 
   public static FlinkKafkaProducer<SojEvent> createKafkaProducer() {
