ab3388a20ae34035d05f96b19bd4bca9c20ed09a
xiaoding <xiaoding@ebay.com>
2020-04-03 19:11:41 +0800

    add qa hdfs path

--- rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/util/HdfsPathConstants.java
+++ rt-pipeline/src/main/java/com/ebay/sojourner/ubd/rt/util/HdfsPathConstants.java
@@ -3,11 +3,11 @@ package com.ebay.sojourner.ubd.rt.util;
 public class HdfsPathConstants {
 
   public static final String EVENT_PATH =
-      AppEnv.config().getHdfs().getNameNode() + "sys/soj/ubd/events/";
+      AppEnv.config().getHdfs().getNameNode() + "events/";
   public static final String SESSION_PATH =
-      AppEnv.config().getHdfs().getNameNode() + "sys/soj/ubd/sessions/";
+      AppEnv.config().getHdfs().getNameNode() + "sessions/";
   public static final String SIGNATURE_PATH =
-      AppEnv.config().getHdfs().getNameNode() + "sys/soj/ubd/signatures/";
+      AppEnv.config().getHdfs().getNameNode() + "signatures/";
   public static final String LATE_EVENT_PATH =
-      AppEnv.config().getHdfs().getNameNode() + "sys/soj/ubd/late_events/";
+      AppEnv.config().getHdfs().getNameNode() + "late_events/";
 }
