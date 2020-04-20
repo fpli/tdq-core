e1ea58c0ea722ff8564d7fcb23289236fef2a302
lipzhu <lipzhu@ebay.com>
2020-04-20 14:48:38 +0800

    replace junit with junit-jupiter

--- rule-test/src/test/java/com/ebay/dss/soj/ubd/rule/BotRuleDescTest.java
+++ rule-test/src/test/java/com/ebay/dss/soj/ubd/rule/BotRuleDescTest.java
@@ -1,15 +1,15 @@
 package com.ebay.dss.soj.ubd.rule;
 
 import java.time.Duration;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BotRuleDescTest {
 
   BotRuleDesc botRuleDesc;
 
-  @Before
+  @BeforeEach
   public void init() {
     String sql = "select 1 as bot_flag from ubi_event";
     botRuleDesc = BotRuleDesc.of(sql);
@@ -17,20 +17,20 @@ public class BotRuleDescTest {
 
   @Test
   public void testToBotRuleDesc() {
-    Assert.assertEquals(new BotRuleDesc(1, "ubi_event", Duration.ofMinutes(10)), botRuleDesc);
+    Assertions.assertEquals(new BotRuleDesc(1, "ubi_event", Duration.ofMinutes(10)), botRuleDesc);
   }
 
   @Test
   public void testGenBeforeScript() {
     String script = BotRuleDesc.genBeforeCheckScript(botRuleDesc);
     String expected = "select * from default.ubi_session_dq_overall";
-    Assert.assertEquals(expected, script);
+    Assertions.assertEquals(expected, script);
   }
 
   @Test
   public void testGenAfterScript() {
     String script = BotRuleDesc.genAfterCheckScript(botRuleDesc);
     String expected = "select * from default.ubi_bot_dq_overall";
-    Assert.assertEquals(expected, script);
+    Assertions.assertEquals(expected, script);
   }
 }
