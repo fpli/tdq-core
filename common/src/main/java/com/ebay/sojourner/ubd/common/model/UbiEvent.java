9933ce23099a031a420f3f6864bf1bf624ca4c6f
xiaoding <xiaoding@ebay.com>
2020-06-03 22:45:16 +0800

    fix NPE issue

--- common/src/main/java/com/ebay/sojourner/ubd/common/model/UbiEvent.java
+++ common/src/main/java/com/ebay/sojourner/ubd/common/model/UbiEvent.java
@@ -105,7 +105,7 @@ public class UbiEvent implements Serializable {
   public void updateSessionId() {
     int charPos = Constants.HEX_DIGITS.length;
     int mask = (1 << 4) - 1;
-    long decimal = eventTimestamp;
+    long decimal = eventTimestamp == null ? 0L : eventTimestamp;
     char[] out = new char[Constants.HEX_DIGITS.length];
 
     Arrays.fill(out, '0');
