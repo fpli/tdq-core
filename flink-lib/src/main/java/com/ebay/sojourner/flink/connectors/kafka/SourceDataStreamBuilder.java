37189ecaada66d1377e288b91c5bf2c2d8c07204
haxi <haxi@ebay.com>
2020-06-10 14:35:56 +0800

    refactor signature hdfs dump

--- flink-lib/src/main/java/com/ebay/sojourner/flink/connectors/kafka/SourceDataStreamBuilder.java
+++ flink-lib/src/main/java/com/ebay/sojourner/flink/connectors/kafka/SourceDataStreamBuilder.java
@@ -1,8 +1,5 @@
 package com.ebay.sojourner.flink.connectors.kafka;
 
-import com.ebay.sojourner.common.model.BotSignature;
-import com.ebay.sojourner.common.util.Property;
-import com.ebay.sojourner.flink.common.env.FlinkEnvUtils;
 import com.ebay.sojourner.flink.common.util.DataCenter;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -19,23 +16,6 @@ public class SourceDataStreamBuilder {
         .slotSharingGroup(slotGroup)
         .name(String.format("Rheos Kafka Consumer From DC: %s, Topic: %s", dc, topic))
         .uid(String.format("source-%s-%s-id", dc, topic));
-
-    return sourceDataStream;
-  }
-
-  public static DataStream<BotSignature> buildForSignature(StreamExecutionEnvironment environment,
-      String topic, String groupId, Integer parallelism) {
-
-    DataStream<BotSignature> sourceDataStream = environment
-        .addSource(KafkaSourceFunction.buildSource(
-            topic,
-            FlinkEnvUtils.getString(Property.KAFKA_COMMON_CONSUMER_BROKERS_DEFAULT),
-            groupId,
-            BotSignature.class))
-        .setParallelism(parallelism)
-        .slotSharingGroup(null)
-        .name(String.format("Rheos Kafka Consumer From RNO, Topic: %s", topic))
-        .uid(String.format("source-%s-id", topic));
 
     return sourceDataStream;
   }
