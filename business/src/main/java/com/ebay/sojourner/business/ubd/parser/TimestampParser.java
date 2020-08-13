4535a0a180066acb6a9a99de772367f97f5b675f
xiaoding <xiaoding@ebay.com>
2020-08-13 19:58:12 +0800

    fix data issue in timestampparser

--- business/src/main/java/com/ebay/sojourner/business/ubd/parser/TimestampParser.java
+++ business/src/main/java/com/ebay/sojourner/business/ubd/parser/TimestampParser.java
@@ -1,10 +1,10 @@
 package com.ebay.sojourner.business.ubd.parser;
 
+import com.ebay.sojourner.common.model.RawEvent;
+import com.ebay.sojourner.common.model.UbiEvent;
 import com.ebay.sojourner.common.util.SOJNVL;
 import com.ebay.sojourner.common.util.SOJTS2Date;
 import com.ebay.sojourner.common.util.SOJURLDecodeEscape;
-import com.ebay.sojourner.common.model.RawEvent;
-import com.ebay.sojourner.common.model.UbiEvent;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,9 +13,11 @@ import java.util.Map;
 import java.util.TimeZone;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 /*
- * Author: yunjzhang
  * Source code: /dw/etl/home/prod/sql/sg_ubi_freq.soj_event_wt1.ins.sql
  * */
 public class TimestampParser implements FieldParser<RawEvent, UbiEvent> {
@@ -23,14 +25,29 @@ public class TimestampParser implements FieldParser<RawEvent, UbiEvent> {
   private static final Logger log = Logger.getLogger(TimestampParser.class);
   private static final long UPPERLIMITMICRO = 1 * 60 * 1000000L; // 2 minutes
   private static final long LOWERLIMITMICRO = -30 * 60 * 1000000L; // 31 minutes
+  private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
   // time zone is GMT-7
   private static final TimeZone timeZone = TimeZone.getTimeZone("GMT-7");
   private static final String P_TAG = "p";
   private static final TimeZone utcTimeZone = TimeZone.getTimeZone("UTC");
+  private DateTimeFormatter formaterUtc = DateTimeFormat.forPattern(DEFAULT_DATE_FORMAT).withZone(
+      DateTimeZone.forTimeZone(utcTimeZone));
+  private DateTimeFormatter formater = DateTimeFormat.forPattern(DEFAULT_DATE_FORMAT).withZone(
+      DateTimeZone.forTimeZone(timeZone));
+
+  public static void main(String[] args) throws ParseException {
+    DateTimeFormatter formaterUtcTest =
+        DateTimeFormat.forPattern(DEFAULT_DATE_FORMAT).withZone(
+            DateTimeZone.forTimeZone(utcTimeZone));
+    String mtstsString = "2020-08-13T09:45:47.079Z";
+    mtstsString = mtstsString.replaceAll("T", " ")
+        .replaceAll("Z", "");
+    System.out.println(mtstsString);
+    System.out.println(
+        SOJTS2Date.getSojTimestamp(formaterUtcTest.parseDateTime(mtstsString).getMillis()));
+  }
 
   public void parse(RawEvent rawEvent, UbiEvent ubiEvent) {
-    SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-    SimpleDateFormat formaterUtc = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
     StringBuilder buffer = new StringBuilder();
     Long abEventTimestamp = null;
     Long eventTimestamp = null;
@@ -38,8 +55,6 @@ public class TimestampParser implements FieldParser<RawEvent, UbiEvent> {
     String applicationPayload = ubiEvent.getApplicationPayload();
     String mtstsString = null;
     String pageId = null;
-    formater.setTimeZone(timeZone);
-    formaterUtc.setTimeZone(utcTimeZone);
     // abEventTimestamp = rawEvent.getAbEventTimestamp();
     // for cal2.0 abeventtimestamp format change(from soj timestamp to EPOCH timestamp)
     String tstamp = rawEvent.getClientData().getTStamp();
@@ -72,7 +87,8 @@ public class TimestampParser implements FieldParser<RawEvent, UbiEvent> {
       if (!StringUtils.isBlank(applicationPayload)) {
         // get mtsts from payload
         mtstsString =
-            SOJURLDecodeEscape.decodeEscapes(SOJNVL.getTagValue(applicationPayload, "mtsts"), '%');
+            SOJURLDecodeEscape.decodeEscapes(
+                SOJNVL.getTagValue(applicationPayload, "mtsts"), '%');
 
         // compare ab_event_timestamp and mtsts
         if (!StringUtils.isBlank(mtstsString) && mtstsString.trim().length() >= 21) {
@@ -86,9 +102,12 @@ public class TimestampParser implements FieldParser<RawEvent, UbiEvent> {
           buffer.setLength(0);
           try {
             if (mtstsString.endsWith("Z")) {
-              eventTimestamp = SOJTS2Date.getSojTimestamp(formaterUtc.parse(mtstsString).getTime());
+              mtstsString = mtstsString.replaceAll("T", " ").replaceAll("Z", "");
+              eventTimestamp =
+                  SOJTS2Date.getSojTimestamp(formaterUtc.parseDateTime(mtstsString).getMillis());
             } else {
-              eventTimestamp = SOJTS2Date.getSojTimestamp(formater.parse(mtstsString).getTime());
+              eventTimestamp = SOJTS2Date
+                  .getSojTimestamp(formater.parseDateTime(mtstsString).getMillis());
             }
             interval = getMicroSecondInterval(eventTimestamp, abEventTimestamp);
             if (interval > UPPERLIMITMICRO || interval < LOWERLIMITMICRO) {
