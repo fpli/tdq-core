3ecf431609dac3e92b2dfbb4c69f0de674f90415
xiaoding <xiaoding@ebay.com>
2020-08-27 14:08:02 +0800

    change time metric

--- rt-pipeline/src/main/java/com/ebay/sojourner/rt/common/metrics/PipelineMetricsCollectorProcessFunction.java
+++ rt-pipeline/src/main/java/com/ebay/sojourner/rt/common/metrics/PipelineMetricsCollectorProcessFunction.java
@@ -4,20 +4,21 @@ import com.codahale.metrics.Histogram;
 import com.codahale.metrics.SlidingWindowReservoir;
 import com.ebay.sojourner.common.model.UbiEvent;
 import com.ebay.sojourner.common.util.Constants;
+import com.ebay.sojourner.common.util.SojTimestamp;
 import java.util.Date;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.dropwizard.metrics.DropwizardHistogramWrapper;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.util.Collector;
 
-public class PipelineMetricsCollectorProcessFunction extends ProcessFunction<UbiEvent,UbiEvent> {
+public class PipelineMetricsCollectorProcessFunction extends ProcessFunction<UbiEvent, UbiEvent> {
 
-  private transient DropwizardHistogramWrapper siteToSourceWrapper;
-  private transient DropwizardHistogramWrapper siteToSinkWrapper;
-  private transient DropwizardHistogramWrapper sourceToSinkWrapper;
   private static final String siteToSource = "site_to_source";
   private static final String siteToSink = "site_to_sink";
   private static final String sourceToSink = "source_to_sink";
+  private transient DropwizardHistogramWrapper siteToSourceWrapper;
+  private transient DropwizardHistogramWrapper siteToSinkWrapper;
+  private transient DropwizardHistogramWrapper sourceToSinkWrapper;
 
   @Override
   public void open(Configuration parameters) throws Exception {
@@ -42,8 +43,11 @@ public class PipelineMetricsCollectorProcessFunction extends ProcessFunction<Ubi
   @Override
   public void processElement(UbiEvent value, Context ctx, Collector<UbiEvent> out) {
     long end = new Date().getTime();
-    long siteToSource = value.getIngestTime() - value.getGenerateTime();
-    long siteToSink = end - value.getGenerateTime();
+    long siteToSource =
+        value.getIngestTime() - SojTimestamp
+            .getSojTimestampToUnixTimestamp(value.getEventTimestamp());
+    long siteToSink = end - SojTimestamp
+        .getSojTimestampToUnixTimestamp(value.getEventTimestamp());
     long sourceToSink = (end - value.getIngestTime());
     siteToSourceWrapper.update(siteToSource);
     siteToSinkWrapper.update(siteToSink);
