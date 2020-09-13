9e8188137950b405a0db89508856f76322920442
xiaoding <xiaoding@ebay.com>
2020-09-13 17:16:32 +0800

    allign column type and name with JET and MR

--- common/src/main/java/com/ebay/sojourner/common/util/SojUtils.java
+++ common/src/main/java/com/ebay/sojourner/common/util/SojUtils.java
@@ -1,6 +1,10 @@
 package com.ebay.sojourner.common.util;
 
+import com.ebay.sojourner.common.model.SojEvent;
+import com.ebay.sojourner.common.model.SojSession;
 import com.ebay.sojourner.common.model.UbiEvent;
+import com.ebay.sojourner.common.model.UbiSession;
+import java.util.ArrayList;
 
 public class SojUtils {
 
@@ -36,5 +40,149 @@ public class SojUtils {
         // || urlQueryString.matches("(/roverimp|.*SojPageView).*")
         && !IntermediateLkp.getInstance().getScPageSet1().contains(pageId)
         && !IntermediateLkp.getInstance().getScPageSet2().contains(pageId);
+  }
+
+  public static SojEvent convertUbiEvent2SojEvent(UbiEvent ubiEvent){
+    SojEvent sojEvent = new SojEvent();
+    sojEvent.setGuid(ubiEvent.getGuid());
+    sojEvent.setAppId(ubiEvent.getAppId());
+    sojEvent.setApplicationPayload(ubiEvent.getApplicationPayload());
+    sojEvent.setAppVersion(ubiEvent.getAppVersion());
+    sojEvent.setBotFlags(new ArrayList<>(ubiEvent.getBotFlags()));
+    sojEvent.setClientData(
+        ubiEvent.getClientData() == null ? null : ubiEvent.getClientData().toString());
+    sojEvent.setBrowserFamily(ubiEvent.getBrowserFamily());
+    sojEvent.setBrowserVersion(ubiEvent.getBrowserVersion());
+    sojEvent.setClickId(String.valueOf(ubiEvent.getClickId()));
+    sojEvent.setClientIP(ubiEvent.getClientIP());
+    sojEvent.setCobrand(String.valueOf(ubiEvent.getCobrand()));
+    sojEvent.setCookies(ubiEvent.getCookies());
+    sojEvent.setCurrentImprId(ubiEvent.getCurrentImprId());
+    sojEvent.setDataCenter(ubiEvent.getDataCenter());
+    sojEvent.setDeviceFamily(ubiEvent.getDeviceFamily());
+    sojEvent.setDeviceType(ubiEvent.getDeviceType());
+    sojEvent.setEnrichedOsVersion(ubiEvent.getEnrichedOsVersion());
+    sojEvent.setEventAction(ubiEvent.getEventAction());
+    sojEvent.setEventCaptureTime(ubiEvent.getEventCaptureTime());
+    sojEvent.setEventAttr(ubiEvent.getEventAttr());
+    sojEvent.setEventCnt(ubiEvent.getEventCnt());
+    sojEvent.setEventFamily(ubiEvent.getEventFamily());
+    sojEvent.setEventTimestamp(ubiEvent.getEventTimestamp());
+    sojEvent.setFlags(ubiEvent.getFlags());
+    sojEvent.setForwardedFor(ubiEvent.getForwardedFor());
+    sojEvent.setGenerateTime(ubiEvent.getGenerateTime());
+    sojEvent.setIcfBinary(ubiEvent.getIcfBinary());
+    sojEvent.setIframe(ubiEvent.isIframe());
+    sojEvent.setIngestTime(ubiEvent.getIngestTime());
+    sojEvent.setItemId(ubiEvent.getItemId()==null? null:String.valueOf(ubiEvent.getItemId()));
+    sojEvent.setOldSessionSkey(ubiEvent.getOldSessionSkey());
+    sojEvent.setOsFamily(ubiEvent.getOsFamily());
+    sojEvent.setOsVersion(ubiEvent.getOsVersion());
+    sojEvent.setPageFamily(ubiEvent.getPageFamily());
+    sojEvent.setPageId(ubiEvent.getPageId());
+    sojEvent.setPageName(ubiEvent.getPageName());
+    sojEvent.setAgentInfo(ubiEvent.getAgentInfo());
+    sojEvent.setPartialValidPage(ubiEvent.isPartialValidPage());
+    sojEvent.setRdt(ubiEvent.isRdt() ? 1 : 0);
+    sojEvent.setRefererHash(ubiEvent.getRefererHash());
+    sojEvent.setReferrer(ubiEvent.getReferrer());
+    sojEvent.setRegu(ubiEvent.getRegu());
+    sojEvent.setRemoteIP(ubiEvent.getRemoteIP());
+    sojEvent.setRequestCorrelationId(ubiEvent.getRequestCorrelationId());
+    sojEvent.setReservedForFuture(ubiEvent.getReservedForFuture());
+    sojEvent.setRlogid(ubiEvent.getRlogid());
+    sojEvent.setSeqNum(ubiEvent.getSeqNum());
+    sojEvent.setSessionEndTime(ubiEvent.getSessionEndTime());
+    sojEvent.setSessionSkey(ubiEvent.getSessionSkey());
+    sojEvent.setSessionId(ubiEvent.getSessionId());
+    sojEvent.setSessionStartDt(ubiEvent.getSessionStartDt());
+    sojEvent.setSojDataDt(ubiEvent.getSojDataDt());
+    sojEvent.setSessionStartTime(ubiEvent.getSessionStartTime());
+    sojEvent.setSid(ubiEvent.getSid());
+    sojEvent.setSiteId(String.valueOf(ubiEvent.getSiteId()));
+    sojEvent.setSourceImprId(ubiEvent.getSourceImprId());
+    sojEvent.setSqr(ubiEvent.getSqr());
+    sojEvent.setStaticPageType(ubiEvent.getStaticPageType());
+    sojEvent.setTrafficSource(ubiEvent.getTrafficSource());
+    sojEvent.setUrlQueryString(ubiEvent.getUrlQueryString());
+    sojEvent.setUserId(ubiEvent.getUserId());
+    sojEvent.setVersion(ubiEvent.getVersion());
+    sojEvent.setWebServer(ubiEvent.getWebServer());
+    return sojEvent;
+  }
+
+  public static SojSession convertUbiSession2SojSession(UbiSession ubiSession){
+    SojSession sojSession = new SojSession();
+    sojSession.setGuid(ubiSession.getGuid());
+    sojSession.setSessionId(ubiSession.getSessionId());
+    sojSession.setSessionSkey(ubiSession.getSessionSkey());
+    sojSession.setIp(ubiSession.getIp());
+    sojSession.setUserAgent(ubiSession.getUserAgent());
+    sojSession.setSojDataDt(ubiSession.getSojDataDt());
+    sojSession.setSessionStartDt(ubiSession.getSessionStartDt());
+    sojSession.setStartTimestamp(ubiSession.getStartTimestamp());
+    sojSession.setEndTimestamp(ubiSession.getEndTimestamp());
+    sojSession.setAbsStartTimestamp(ubiSession.getAbsStartTimestamp());
+    sojSession.setAbsEndTimestamp(ubiSession.getAbsEndTimestamp());
+    sojSession.setBotFlagList(new ArrayList<>(ubiSession.getBotFlagList()));
+    sojSession.setNonIframeRdtEventCnt(ubiSession.getNonIframeRdtEventCnt());
+    sojSession.setSessionReferrer(ubiSession.getSessionReferrer());
+    sojSession.setBotFlag(RulePriorityUtils.getHighPriorityBotFlag(ubiSession.getBotFlagList()));
+    sojSession.setVersion(ubiSession.getVersion());
+    sojSession.setFirstUserId(ubiSession.getFirstUserId());
+    sojSession.setSiteFlags(ubiSession.getSiteFlags());
+    sojSession.setAttrFlags(ubiSession.getAttrFlags());
+    sojSession.setBotFlags(ubiSession.getBotFlags());
+    sojSession.setFindingFlags(ubiSession.getFindingFlags());
+    sojSession.setStartPageId(ubiSession.getStartPageId());
+    sojSession.setEndPageId(ubiSession.getEndPageId());
+    sojSession.setDurationSec(ubiSession.getDurationSec());
+    sojSession.setEventCnt(ubiSession.getEventCnt());
+    sojSession.setAbsEventCnt(ubiSession.getAbsEventCnt());
+    sojSession.setViCoreCnt(ubiSession.getViCoreCnt());
+    sojSession.setBidCoreCnt(ubiSession.getBidCoreCnt());
+    sojSession.setBinCoreCnt(ubiSession.getBinCoreCnt());
+    sojSession.setWatchCoreCnt(ubiSession.getWatchCoreCnt());
+    sojSession.setTrafficSrcId(ubiSession.getTrafficSrcId());
+    sojSession.setAbsDuration(ubiSession.getAbsDuration());
+    sojSession.setCobrand(ubiSession.getCobrand());
+    sojSession.setFirstAppId(ubiSession.getFirstAppId());
+    sojSession.setFirstSiteId(ubiSession.getFirstSiteId());
+    sojSession.setFirstCguid(ubiSession.getFirstCguid());
+    sojSession.setFirstMappedUserId(ubiSession.getFirstMappedUserId());
+    sojSession.setHomepageCnt(ubiSession.getHomepageCnt());
+    sojSession.setGr1Cnt(ubiSession.getGr1Cnt());
+    sojSession.setGrCnt(ubiSession.getGrCnt());
+    sojSession.setMyebayCnt(ubiSession.getMyebayCnt());
+    sojSession.setSigninPageCnt(ubiSession.getSigninPageCnt());
+    sojSession.setFirstSessionStartDt(ubiSession.getFirstSessionStartDt());
+    sojSession.setSingleClickSessionFlag(ubiSession.getSingleClickSessionFlag());
+    sojSession.setAsqCnt(ubiSession.getAsqCnt());
+    sojSession.setAtcCnt(ubiSession.getAtcCnt());
+    sojSession.setAtlCnt(ubiSession.getAtlCnt());
+    sojSession.setBoCnt(ubiSession.getBoCnt());
+    sojSession.setSrpCnt(ubiSession.getSrpCnt());
+    sojSession.setServEventCnt(ubiSession.getServEventCnt());
+    sojSession.setSearchViewPageCnt(ubiSession.getSearchViewPageCnt());
+    sojSession.setBrowserFamily(ubiSession.getBrowserFamily());
+    sojSession.setBrowserVersion(ubiSession.getBrowserVersion());
+    sojSession.setCity(ubiSession.getCity());
+    sojSession.setContinent(ubiSession.getContinent());
+    sojSession.setCountry(ubiSession.getCountry());
+    sojSession.setDeviceClass(ubiSession.getDeviceClass());
+    sojSession.setDeviceFamily(ubiSession.getDeviceFamily());
+    sojSession.setEndResourceId(ubiSession.getEndResourceId());
+    sojSession.setIsReturningVisitor(ubiSession.isReturningVisitor());
+    sojSession.setLineSpeed(ubiSession.getLineSpeed());
+    sojSession.setOsFamily(ubiSession.getOsFamily());
+    sojSession.setOsVersion(ubiSession.getOsVersion());
+    sojSession.setPulsarEventCnt(ubiSession.getPulsarEventCnt());
+    sojSession.setRegion(ubiSession.getRegion());
+    sojSession.setSessionEndDt(ubiSession.getSessionEndDt());
+    sojSession.setStartResourceId(ubiSession.getStartResourceId());
+    sojSession.setStreamId(ubiSession.getStreamId());
+    sojSession.setBuserId(ubiSession.getBuserId());
+    sojSession.setIsOpen(ubiSession.isOpenEmit());
+    return sojSession;
   }
 }
--- rt-pipeline/src/main/java/com/ebay/sojourner/rt/operator/session/UbiSessionToSojSessionProcessFunction.java
+++ rt-pipeline/src/main/java/com/ebay/sojourner/rt/operator/session/UbiSessionToSojSessionProcessFunction.java
@@ -2,8 +2,7 @@ package com.ebay.sojourner.rt.operator.session;
 
 import com.ebay.sojourner.common.model.SojSession;
 import com.ebay.sojourner.common.model.UbiSession;
-import com.ebay.sojourner.common.util.RulePriorityUtils;
-import java.util.ArrayList;
+import com.ebay.sojourner.common.util.SojUtils;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.commons.collections.CollectionUtils;
@@ -29,77 +28,7 @@ public class UbiSessionToSojSessionProcessFunction extends ProcessFunction<UbiSe
   @Override
   public void processElement(UbiSession ubiSession, Context context, Collector<SojSession> out)
       throws Exception {
-    SojSession sojSession = new SojSession();
-    sojSession.setGuid(ubiSession.getGuid());
-    sojSession.setSessionId(ubiSession.getSessionId());
-    sojSession.setSessionSkey(ubiSession.getSessionSkey());
-    sojSession.setIp(ubiSession.getIp());
-    sojSession.setUserAgent(ubiSession.getUserAgent());
-    sojSession.setSojDataDt(ubiSession.getSojDataDt());
-    sojSession.setSessionStartDt(ubiSession.getSessionStartDt());
-    sojSession.setStartTimestamp(ubiSession.getStartTimestamp());
-    sojSession.setEndTimestamp(ubiSession.getEndTimestamp());
-    sojSession.setAbsStartTimestamp(ubiSession.getAbsStartTimestamp());
-    sojSession.setAbsEndTimestamp(ubiSession.getAbsEndTimestamp());
-    sojSession.setBotFlagList(new ArrayList<>(ubiSession.getBotFlagList()));
-    sojSession.setNonIframeRdtEventCnt(ubiSession.getNonIframeRdtEventCnt());
-    sojSession.setSessionReferrer(ubiSession.getSessionReferrer());
-    sojSession.setBotFlag(RulePriorityUtils.getHighPriorityBotFlag(ubiSession.getBotFlagList()));
-    sojSession.setVersion(ubiSession.getVersion());
-    sojSession.setFirstUserId(ubiSession.getFirstUserId());
-    sojSession.setSiteFlags(ubiSession.getSiteFlags());
-    sojSession.setAttrFlags(ubiSession.getAttrFlags());
-    sojSession.setBotFlags(ubiSession.getBotFlags());
-    sojSession.setFindingFlags(ubiSession.getFindingFlags());
-    sojSession.setStartPageId(ubiSession.getStartPageId());
-    sojSession.setEndPageId(ubiSession.getEndPageId());
-    sojSession.setDurationSec(ubiSession.getDurationSec());
-    sojSession.setEventCnt(ubiSession.getEventCnt());
-    sojSession.setAbsEventCnt(ubiSession.getAbsEventCnt());
-    sojSession.setViCoreCnt(ubiSession.getViCoreCnt());
-    sojSession.setBidCoreCnt(ubiSession.getBidCoreCnt());
-    sojSession.setBinCoreCnt(ubiSession.getBinCoreCnt());
-    sojSession.setWatchCoreCnt(ubiSession.getWatchCoreCnt());
-    sojSession.setTrafficSrcId(ubiSession.getTrafficSrcId());
-    sojSession.setAbsDuration(ubiSession.getAbsDuration());
-    sojSession.setCobrand(ubiSession.getCobrand());
-    sojSession.setFirstAppId(ubiSession.getFirstAppId());
-    sojSession.setFirstSiteId(ubiSession.getFirstSiteId());
-    sojSession.setFirstCguid(ubiSession.getFirstCguid());
-    sojSession.setFirstMappedUserId(ubiSession.getFirstMappedUserId());
-    sojSession.setHomepageCnt(ubiSession.getHomepageCnt());
-    sojSession.setGr1Cnt(ubiSession.getGr1Cnt());
-    sojSession.setGrCnt(ubiSession.getGrCnt());
-    sojSession.setMyebayCnt(ubiSession.getMyebayCnt());
-    sojSession.setSigninPageCnt(ubiSession.getSigninPageCnt());
-    sojSession.setFirstSessionStartDt(ubiSession.getFirstSessionStartDt());
-    sojSession.setSingleClickSessionFlag(ubiSession.getSingleClickSessionFlag());
-    sojSession.setAsqCnt(ubiSession.getAsqCnt());
-    sojSession.setAtcCnt(ubiSession.getAtcCnt());
-    sojSession.setAtlCnt(ubiSession.getAtlCnt());
-    sojSession.setBoCnt(ubiSession.getBoCnt());
-    sojSession.setSrpCnt(ubiSession.getSrpCnt());
-    sojSession.setServEventCnt(ubiSession.getServEventCnt());
-    sojSession.setSearchViewPageCnt(ubiSession.getSearchViewPageCnt());
-    sojSession.setBrowserFamily(ubiSession.getBrowserFamily());
-    sojSession.setBrowserVersion(ubiSession.getBrowserVersion());
-    sojSession.setCity(ubiSession.getCity());
-    sojSession.setContinent(ubiSession.getContinent());
-    sojSession.setCountry(ubiSession.getCountry());
-    sojSession.setDeviceClass(ubiSession.getDeviceClass());
-    sojSession.setDeviceFamily(ubiSession.getDeviceFamily());
-    sojSession.setEndResourceId(ubiSession.getEndResourceId());
-    sojSession.setIsReturningVisitor(ubiSession.isReturningVisitor());
-    sojSession.setLineSpeed(ubiSession.getLineSpeed());
-    sojSession.setOsFamily(ubiSession.getOsFamily());
-    sojSession.setOsVersion(ubiSession.getOsVersion());
-    sojSession.setPulsarEventCnt(ubiSession.getPulsarEventCnt());
-    sojSession.setRegion(ubiSession.getRegion());
-    sojSession.setSessionEndDt(ubiSession.getSessionEndDt());
-    sojSession.setStartResourceId(ubiSession.getStartResourceId());
-    sojSession.setStreamId(ubiSession.getStreamId());
-    sojSession.setBuserId(ubiSession.getBuserId());
-    sojSession.setIsOpen(ubiSession.isOpenEmit());
+    SojSession sojSession = SojUtils.convertUbiSession2SojSession(ubiSession);
 
     // split bot session and nonbot session
     if (sojSession.getBotFlagList().size() == 0 || CollectionUtils
