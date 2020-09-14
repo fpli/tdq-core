137b65634178fff247c5d4a9957132d6db6d5514
haxi <haxi@ebay.com>
2020-09-14 17:14:00 +0800

    rename property key for watemark process progress metric

--- common/src/main/java/com/ebay/sojourner/common/util/Property.java
+++ common/src/main/java/com/ebay/sojourner/common/util/Property.java
@@ -247,7 +247,7 @@ public class Property {
   public static final String FLINK_APP_NAME = "flink.app.name";
 
   // flink - metric name
-  public static final String FLINK_APP_METRIC_NAME = "flink.app.metric.name";
+  public static final String FLINK_APP_METRIC_NAME = "flink.app.metric.watermark-process-progress";
 
   // zookeeper
   public static final String ZOOKEEPER_SERVER = "zookeeper.server";
