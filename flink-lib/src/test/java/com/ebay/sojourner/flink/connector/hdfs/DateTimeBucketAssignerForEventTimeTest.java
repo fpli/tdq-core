3ac5373337d4aa284644916e244339093f6dffc1
haxi <haxi@ebay.com>
2020-11-12 11:28:47 +0800

    fix bucket assginer unit test

--- flink-lib/src/test/java/com/ebay/sojourner/flink/connector/hdfs/DateTimeBucketAssignerForEventTimeTest.java
+++ flink-lib/src/test/java/com/ebay/sojourner/flink/connector/hdfs/DateTimeBucketAssignerForEventTimeTest.java
@@ -3,6 +3,8 @@ package com.ebay.sojourner.flink.connector.hdfs;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.ebay.sojourner.common.model.SojSession;
+import com.ebay.sojourner.common.model.UbiSession;
+import com.ebay.sojourner.common.util.SojUtils;
 import org.apache.flink.streaming.api.functions.sink.filesystem.BucketAssigner.Context;
 import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,13 +14,17 @@ class DateTimeBucketAssignerForEventTimeTest {
 
   DateTimeBucketAssignerForEventTime<SojSession> bucketAssigner;
   SojSession sojSession;
+  UbiSession ubiSession;
   Context context;
 
   @BeforeEach
   void setUp() {
     bucketAssigner = new DateTimeBucketAssignerForEventTime<>();
-    sojSession = new SojSession();
-    sojSession.setSessionStartDt(1605150999996L);
+    ubiSession = new UbiSession();
+    ubiSession.setSessionStartDt(3804278400000000L);
+    ubiSession.setAbsStartTimestamp(3804278400000000L);
+    sojSession = SojUtils.convertUbiSession2SojSession(ubiSession);
+
     context = new Context() {
       @Override
       public long currentProcessingTime() {
@@ -41,7 +47,7 @@ class DateTimeBucketAssignerForEventTimeTest {
   @Test
   void getBucketId() {
     String bucketId = bucketAssigner.getBucketId(sojSession, context);
-    assertThat(bucketId).isEqualTo("dt=20201111/hr=20");
+    assertThat(bucketId).isEqualTo("dt=20200721/hr=00");
   }
 
   @Test
