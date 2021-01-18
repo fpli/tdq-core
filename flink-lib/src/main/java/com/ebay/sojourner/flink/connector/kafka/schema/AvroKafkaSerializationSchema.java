913200716196179577da3f6eee6ba1bb5c22ba3a
Jason Xiao <yxiao6@ebay.com>
2021-01-18 14:40:29 +0800

    remove schema field in AvroKafkaSerializationSchema constructor

--- flink-lib/src/main/java/com/ebay/sojourner/flink/connector/kafka/schema/AvroKafkaSerializationSchema.java
+++ flink-lib/src/main/java/com/ebay/sojourner/flink/connector/kafka/schema/AvroKafkaSerializationSchema.java
@@ -3,7 +3,6 @@ package com.ebay.sojourner.flink.connector.kafka.schema;
 import com.ebay.sojourner.flink.connector.kafka.AvroKafkaSerializer;
 import com.ebay.sojourner.flink.connector.kafka.KafkaSerializer;
 import java.util.List;
-import org.apache.avro.Schema;
 import org.apache.avro.specific.SpecificRecord;
 import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,20 +12,18 @@ public class AvroKafkaSerializationSchema<T extends SpecificRecord> implements
     KafkaSerializationSchema<T> {
 
   public final String defaultTopic;
-  private final Schema schema;
   private final List<String> keyFields;
   private transient KafkaSerializer<T> kafkaSerializer;
 
-  public AvroKafkaSerializationSchema(String defaultTopic, Schema schema, List<String> keyFields) {
+  public AvroKafkaSerializationSchema(String defaultTopic, List<String> keyFields) {
     this.defaultTopic = defaultTopic;
-    this.schema = schema;
     this.keyFields = keyFields;
   }
 
   @Override
   public ProducerRecord<byte[], byte[]> serialize(T element, @Nullable Long timestamp) {
     if (kafkaSerializer == null) {
-      kafkaSerializer = new AvroKafkaSerializer<>(schema);
+      kafkaSerializer = new AvroKafkaSerializer<>(element.getSchema());
     }
     return new ProducerRecord<>(defaultTopic,
                                 kafkaSerializer.encodeKey(element, keyFields),
