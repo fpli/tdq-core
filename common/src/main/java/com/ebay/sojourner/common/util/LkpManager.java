176f5946dbde8cb5da11efba43a80ae3b8c306c9
daviddingly <xiaoding@ebay.com>
2021-02-24 22:04:19 +0800

    add refresh pagefmlyall

--- common/src/main/java/com/ebay/sojourner/common/util/LkpManager.java
+++ common/src/main/java/com/ebay/sojourner/common/util/LkpManager.java
@@ -96,6 +96,7 @@ public class LkpManager {
         refreshAppIds();
         refreshPageFmlys();
         refreshMpxRotetion();
+        refreshPageFmlysTotal();
         refreshLargeSessionGuid();
         printLkpFileStatus();
         printLkpFileSize();
