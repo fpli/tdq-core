e7477e91f84db95a00f7ed8dea94af1af846e651
juntao zhang <juntzhang@ebay.com>
2021-10-28 16:02:52 +0800

    UTDQ-1040

--- tdq-job/src/main/java/com/ebay/tdq/connector/kafka/schema/TdqEventDeserializationSchema.java
+++ tdq-job/src/main/java/com/ebay/tdq/connector/kafka/schema/TdqEventDeserializationSchema.java
@@ -7,6 +7,8 @@ import com.ebay.tdq.expressions.Expression;
 import com.ebay.tdq.rules.ExpressionParser;
 import com.ebay.tdq.rules.PhysicalPlan;
 import io.ebay.rheos.schema.event.RheosEvent;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
@@ -14,6 +16,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.metrics.Counter;
+import org.apache.flink.metrics.MetricGroup;
 
 @Slf4j
 public class TdqEventDeserializationSchema extends AbstractTdqEventDeserializationSchema implements
@@ -22,9 +26,13 @@ public class TdqEventDeserializationSchema extends AbstractTdqEventDeserializati
   private final String schemaRegistryUrl;
   private final Long endTimestamp;
   private final Expression eventTimeExpr;
+  private transient Map<String, Counter> counterMap;
+  private transient MetricGroup group;
+  private transient long errorMsgCurrentTimeMillis = 0L;
+  private final String name;
 
   public TdqEventDeserializationSchema(KafkaSourceConfig ksc, TdqEnv tdqEnv) {
-    super(ksc.getName());
+    this.name = ksc.getName();
     this.schemaRegistryUrl = ksc.getRheosServicesUrls();
     this.endTimestamp = ksc.getEndOfStreamTimestamp();
     Validate.isTrue(StringUtils.isNotBlank(ksc.getSchemaSubject()), "schema-subject is empty!");
@@ -36,7 +44,11 @@ public class TdqEventDeserializationSchema extends AbstractTdqEventDeserializati
 
   @Override
   public void open(InitializationContext context) {
-    super.open(context.getMetricGroup());
+    this.group = context.getMetricGroup();
+    counterMap = new HashMap<>();
+    this.group = group.addGroup("tdq").addGroup("src", name);
+    errorMsgCurrentTimeMillis = 0L;
+    log.info("open success");
   }
 
   @Override
@@ -69,6 +81,26 @@ public class TdqEventDeserializationSchema extends AbstractTdqEventDeserializati
 
   public boolean isEndOfStream(long t) {
     return this.endTimestamp > 0 && t > this.endTimestamp;
+  }
+
+  @Override
+  Map<String, Counter> getCounterMap() {
+    return counterMap;
+  }
+
+  @Override
+  MetricGroup getGroup() {
+    return group;
+  }
+
+  @Override
+  long getErrorMsgCurrentTimeMillis() {
+    return errorMsgCurrentTimeMillis;
+  }
+
+  @Override
+  void setErrorMsgCurrentTimeMillis(Long t) {
+    this.errorMsgCurrentTimeMillis = t;
   }
 
 }
